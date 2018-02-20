1a09d28acb58132c95929b1d3b4436c09f3291d2
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-20 19:06:13 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,10 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      all.clear
+    end
   
-  def reset_all
-    all.clear
-  end
-
-  def count
-    all.count
-  end
+    def count
+      all.count
+    end
 end
