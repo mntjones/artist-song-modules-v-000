c5aa6a30c72537d1b8a29d9c5b08b9e71949f339
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-20 19:00:01 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,14 +4,8 @@ class Artist
   extend Memorable, Findable
   attr_accessor :name
   attr_reader :songs
-  
-  
 
   @@artists = []
-
-  #def self.find_by_name(name)
-  #  @@artists.detect{|a| a.name == name}
-  #end
 
   def initialize
     @@artists << self
